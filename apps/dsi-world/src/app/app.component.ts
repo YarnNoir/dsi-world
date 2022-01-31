21c17036a5aae3e0c5f2a49648173c0354c5a8db
yarnnoir <billyoneid@outlook.co.id>
2022-01-31 19:20:46 +0700

    totality init

---
+++ apps/dsi-world/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dsi-world-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'dsi-world';
+}
